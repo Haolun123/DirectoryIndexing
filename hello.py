75a02dbffe1dbf45bb697b9fb90809098b50e89e
Haolun Yan <haolun.yan@sap.com>
2020-05-18 23:10:06 +0800

    Extra code

--- hello.py
+++ hello.py
@@ -51,16 +51,42 @@ class OrgFile:
                 print(update_value)
                 self.file_count_dict.update({new_file_dir: update_value + 1})
             print(self.file_count_dict)
-            shutil.copy(file, os.path.join(self.root_path, regex.sub(r'\.mp4$', '', os.path.basename(file)))
+            shutil.move(file, os.path.join(self.root_path, regex.sub(r'\.mp4$', '', os.path.basename(file)))
                         + "/" + regex.sub(r'\.mp4$', '', os.path.basename(file)) + "_"
                         + str(self.file_count_dict.get(new_file_dir)) + ".mp4")
         for file in pics_list:
             path = os.path.join(self.root_path, regex.sub(r'\.jpeg$', '', os.path.basename(file)))
             if os.path.exists(path):
-                shutil.copy(file, path)
+                shutil.move(file, path)
             else:
                 os.makedirs(extra_pics_path, exist_ok=True)
-                shutil.copy(file, extra_pics_path)
+                shutil.move(file, extra_pics_path)
+
+    @staticmethod
+    def file_util(directory):
+        root_dir_list = os.listdir(directory)
+        for ele in root_dir_list:
+            if os.path.isdir(directory+ele):
+                sub_dir_list = os.listdir(ele)
+                for sub_ele in sub_dir_list:
+                    if not sub_ele.startswith(".") and sub_ele.endswith("mp4"):
+                        shutil.move(directory+ele+"/"+sub_ele, directory+ele+"/"+sub_ele[:-4])
+        for ele in root_dir_list:
+            if os.path.isdir(directory + ele):
+                sub_dir_list = os.listdir(ele)
+                for sub_ele in sub_dir_list:
+                    if os.path.isdir(directory+ele+"/"+sub_ele):
+                        shutil.move(directory+ele+"/"+sub_ele, directory)
+
+    @staticmethod
+    def remove_trash_files(directory):
+        file_list = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(directory)) for f in fn]
+        for file in file_list:
+            if file.endswith(".torrent") or file.endswith(".js"):
+                try:
+                    os.remove(file)
+                except OSError:
+                    continue
 
 
 if __name__ == "__main__":
